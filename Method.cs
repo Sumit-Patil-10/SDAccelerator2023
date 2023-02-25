4afb56487b32acd7f1d872a90021bffea680deb1
Sumit-Patil-10 <85681868+Sumit-Patil-10@users.noreply.github.com>
2023-02-25 18:40:37 +0530

    Update Method.cs

--- Method.cs
+++ Method.cs
@@ -1,15 +1,49 @@
+/* Simple program to calculatesalary
 
-//++This is section for methood
+Concepts Covered 
+- Helper file
+- Optional Parameter
+- Expression bodied syntax
+- string interpolation 
+- Top level Statement feature
+- Named argument
+*/
 
 
+using Method;
 
-int firstValue = 10;
-int secondValue = 20;
+int amount =  15000;
+int totalMonth = 8;
+int incentive = 123;
+
+int Salary = Class1.CalculateYearSalary(amount, totalMonth);
+
+Console.WriteLine($"Yearly Salary + incentive == {Salary + incentive}");
+ static int SimpleAddition(int firstValue, int secondValue) => firstValue + secondValue; // Expression bodied expression
+
+SimpleAddition(firstValue: 10, secondValue: 20);
 
 
-MyMethood(firstValue,secondValue);
-static void MyMethood( int a, int  b)
+/*
+Helper file (Class1.cs)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
 {
-    Console.WriteLine($"addition is:-{a +b }"); // Used string interpolation
-} 
+    internal class Class1
+    {
+
+        public static int CalculateYearSalary(int Salary, int NoOfMonthsWorked, int bonus = 123)
+        {
+            return (Salary * NoOfMonthsWorked);
+        }
+
+    }
+}
 
