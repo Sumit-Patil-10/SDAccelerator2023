435ce29c626763fd8e91d49845d25db47e0be78a
Sumit-Patil-10 <85681868+Sumit-Patil-10@users.noreply.github.com>
2023-02-25 16:39:54 +0530

    Create Method.cs

---
+++ Method.cs
@@ -0,0 +1,15 @@
+
+//++This is section for methood
+
+
+
+int firstValue = 10;
+int secondValue = 20;
+
+
+MyMethood(firstValue,secondValue);
+static void MyMethood( int a, int  b)
+{
+    Console.WriteLine($"addition is:-{a +b }");
+}
+
