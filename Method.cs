9a532a2ae487dccf79c1562441f56e81f7e25dd6
Sumit-Patil-10 <85681868+Sumit-Patil-10@users.noreply.github.com>
2023-02-25 16:45:46 +0530

    Update Method.cs

--- Method.cs
+++ Method.cs
@@ -10,6 +10,6 @@ int secondValue = 20;
 MyMethood(firstValue,secondValue);
 static void MyMethood( int a, int  b)
 {
-    Console.WriteLine($"addition is:-{a +b }");
-}
+    Console.WriteLine($"addition is:-{a +b }"); // Used string interpolation
+} 
 
